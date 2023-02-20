4b6b4ac8f3b8d6f5f4c07ccd89e209ef47fdde32
Kristian Dotson <KDotson387@gmail.com>
2023-02-20 14:37:54 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,6 @@
-let name = prompt("p1")
+let Name = prompt("Hello! What's your name?")
+let sport = prompt("What's your favorite Sport?")
+let movie = prompt("What's your favorite movie or tv series?")
+let role = prompt("Who's your role model?")
+
+alert("Hello" + Name + "my favorite sport is" + sport + "too" + "")
