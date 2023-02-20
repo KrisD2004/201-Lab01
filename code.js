2eb5c31bb0ea29777c700b376069c56374598447
Kristian Dotson <KDotson387@gmail.com>
2023-02-20 14:41:16 -0600

    change sentence

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 let Name = prompt("Hello! What's your name?")
 let sport = prompt("What's your favorite Sport?")
-let movie = prompt("What's your favorite movie or tv series?")
+let movie = prompt("What's your favorite movie?")
 let role = prompt("Who's your role model?")
 
-alert("Hello" + Name + "my favorite sport is" + sport + "too" + "")
+alert("Hello" + Name + "my favorite sport is" + sport + "too" + "My favortie Movie")
