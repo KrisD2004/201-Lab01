98886c6fb68bf57508b5bfbeafb85d7fdf1347e8
Kristian Dotson <KDotson387@gmail.com>
2023-02-20 14:58:39 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,12 @@
 function Questions() {
     let Name = prompt("Hello! What's your name?")
- alert("Nice to meet you" + " " + Name)
+ alert("Nice to meet you " + " " + Name)
  let sport = prompt("What's your favorite Sport?")
- alert("That's cool!" + " " + sport)
+ alert("That's cool! " + " " + sport)
  let movie = prompt("What's your favorite movie?")
- alert("That's a great movie!" + " " + movie)
+ alert("That's a great movie! " + " " + movie)
  let role = prompt("Who's your role model?")
- alert("That's cool thats your role model" + " " + role)
+ alert("That's cool thats your role model " + " " + role)
 
  
 }
